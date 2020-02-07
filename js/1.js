2cb0eb797e9afbf0186a3f36dd595c4928bfc9ee
1760322 <49802190+1760322@users.noreply.github.com>
2020-02-07 12:57:59 +0700

    them product

--- js/1.js
+++ js/1.js
@@ -1,15 +1,16 @@
 $(function(){
+
     var pageWidth = $( window ).width();
    
-        $('.cacngonngu').slideUp();
+        $('.cacngonngu').slideUp(400);
         $('.top-ngonngu b').click(function(event){
           
             $(this).next().slideToggle();
     
         })
-    $('.timkiem').slideUp();
+    $('.timkiem').slideUp(5000);
     $('.iconSearch').click(function(event){
         $('.timkiem').slideToggle();
     })
-
+    new WOW().init();
 })
